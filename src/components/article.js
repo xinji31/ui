8432774f68aa34fb0b05eac8088af68f01536360
asd-a <hjh-asd@outlook.com>
2024-04-09 13:33:43 +0800

    feat: add view article
    feat: add view article
    
    feat: fix getArticleTitle
    
    feat: add getArticle
    
    feat: add error handler to getArticle
    
    feat:update previewList
    
    feat: fetch json
    
    feat: add getSiteInfo
    
    feat: prettify await
    
    prettify js name
    
    feat: complete component article
    
    feat: fix regexp
    
    feat: update file type
    
    feat: add parser
    
    fix(article)

---
+++ src/components/article.js
@@ -0,0 +1,46 @@
+import { BoxComputed, BoxValue } from "../lib/box"
+import { element as e } from "../lib/element"
+import { parse } from "marked"
+
+const siteinfo = {
+  "previewList": [{ "title": "Markdown 语法测试", "tags": ["test", "markdown"], "hash": "dc855b37a7309874c735e65d5d8ba3a829d1e3c8", "path": "src/markdown-test.md", "date": "2024-04-08 16:02:25 +0800", "author": "asd-a" }, { "title": "测试文章", "tags": ["数学,数学分析"], "hash": "ac79123e5510333e76811814c5ae4695776217b0", "path": "src/Assignment%208.pdf", "date": "2024-04-07 20:28:04 +0800", "author": "CoderOJ" }, { "title": "FAQ", "tags": ["site"], "hash": "4cf26ff0746aef286ed80475ef23a18aa28a5fe9", "path": "src/FAQ.md", "date": "2024-04-07 20:20:22 +0800", "author": "CoderOJ" }]
+} // await (await fetch("https://xinji31.github.io/book-test/site-info.json")).json()
+
+function getArticle(id) {
+  for (const art of siteinfo.previewList) {
+    if (art.hash === id) {
+      return art
+    }
+  }
+  throw Error("Article not found")
+}
+
+export function getArticleTitle(id) {
+  return getArticle(id).title
+}
+
+export function getArticleBody(id) {
+  const art = getArticle(id)
+  const type = art.path.match(/\.(md|pdf)$/)[1]
+  const srcURL = `https://raw.githubusercontent.com/xinji31/book-test/${art.hash}/${art.path}`
+
+  if (type === "md") {
+    const artContent = new BoxValue("loading...");
+    (async () => {
+      await new Promise(r => setTimeout(r, 1000))
+      artContent.value = await (await fetch(srcURL)).text()
+    })()
+
+    return e("div").sub(new BoxComputed($ => {
+      const res = e("div")
+      res.innerHTML = parse($(artContent))
+      return res
+    }))
+  }
+  else if (type === "pdf") {
+    return e("a").attr({ href: srcURL }).sub("link")
+  }
+  else {
+    throw Error("Not supported file type")
+  }
+}
--- src/main.js
+++ src/main.js
@@ -2,21 +2,20 @@ import { pageTitle } from "./components/pageTitle"
 import { sidetools } from "./components/sidetools"
 import { element as e } from "./lib/element"
 import { router } from "./lib/router"
-
-function getPreviewList() {
-  return []
-}
+import { getArticleTitle, getArticleBody } from "./components/article"
 
 const navigator = {
   home: s => s === "/",
   publishArticle: s => s.startsWith("/publish/article"),
   publishDiscuss: s => s.startsWith("/publish/discuss"),
+  viewArticle: s => s.startsWith("/view/article"),
 }
 
 const titleText = router(
   [navigator.home, "小绿书"],
   [navigator.publishArticle, "小绿书 - 发布文章"],
   [navigator.publishDiscuss, "小绿书 - 发布讨论"],
+  [navigator.viewArticle, (url => "小绿书 - " + getArticleTitle(url.split("/")[3]))],
 )
 
 document.querySelector("title").sub(titleText)
@@ -29,6 +28,7 @@ document.querySelector("#app").sub(
       [navigator.home, ""],
       [navigator.publishArticle, "publish article"],
       [navigator.publishDiscuss, "publish discuss"],
+      [navigator.viewArticle, (url => getArticleBody(url.split("/")[3]))],
     ),
     sidetools(),
   )
