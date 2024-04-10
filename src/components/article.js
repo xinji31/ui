f53734f17bbbeb7a1e7d92022a4acbe1a9551598
asd-a <hjh-asd@outlook.com>
2024-04-10 13:36:12 +0800

    upd(article): add github-markdown-css

--- src/components/article.js
+++ src/components/article.js
@@ -1,6 +1,7 @@
 import { BoxComputed, BoxValue, boxPromise } from "../lib/box"
 import { element as e } from "../lib/element"
 import { parse } from "marked"
+import "github-markdown-css"
 
 const siteinfo = {
   "previewList": [{ "title": "Markdown 语法测试", "tags": ["test", "markdown"], "hash": "dc855b37a7309874c735e65d5d8ba3a829d1e3c8", "path": "src/markdown-test.md", "date": "2024-04-08 16:02:25 +0800", "author": "asd-a" }, { "title": "测试文章", "tags": ["数学,数学分析"], "hash": "ac79123e5510333e76811814c5ae4695776217b0", "path": "src/Assignment%208.pdf", "date": "2024-04-07 20:28:04 +0800", "author": "CoderOJ" }, { "title": "FAQ", "tags": ["site"], "hash": "4cf26ff0746aef286ed80475ef23a18aa28a5fe9", "path": "src/FAQ.md", "date": "2024-04-07 20:20:22 +0800", "author": "CoderOJ" }]
@@ -29,7 +30,9 @@ export function getArticleBody(id) {
       await new Promise(r => setTimeout(r, 1000))
       const res = e("div")
       res.innerHTML = parse(await (await fetch(srcURL)).text())
-      return res
+      return res.attr({
+        class: "markdown-body",
+      })
     })()))
   }
   else if (type === "pdf") {
